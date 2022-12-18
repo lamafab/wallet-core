a1b3fbbe1fb918198a2f87c177dd97515a7cf5c8
lamafab <42901763+lamafab@users.noreply.github.com>
2022-12-18 15:45:28 +0000

    rename test module to driver_basics

---
+++ codegen_fabio/src/tests/driver_basics.rs
@@ -0,0 +1,43 @@
+use crate::{Error, Walker};
+
+#[test]
+fn read_until() {
+    let sample = "this is\nsome\ndata\n";
+    let mut walker = Walker::new(sample.as_bytes());
+
+    // Returns the value multiple times until `next` is called.
+    assert_eq!("this is", walker.read_until('\n').unwrap());
+    assert_eq!("this is", walker.read_until('\n').unwrap());
+    assert_eq!("this is", walker.read_until('\n').unwrap());
+    walker.next();
+
+    assert_eq!("some", walker.read_until('\n').unwrap());
+    walker.next();
+
+    assert_eq!("data", walker.read_until('\n').unwrap());
+    walker.next();
+}
+
+#[test]
+fn read_until_separator() {
+    let sample = " some\n";
+    let mut walker = Walker::new(sample.as_bytes());
+
+    // `read_until_separator` allows EOF
+    assert_eq!("some", walker.read_until_separator().unwrap());
+    walker.next();
+
+    // Returns empty value after
+    assert_eq!("", walker.read_until_separator().unwrap());
+    walker.next();
+}
+
+#[test]
+fn read_until_eof_error() {
+    let sample = "this is some data";
+    let mut walker = Walker::new(sample.as_bytes());
+
+    // Does not allow EOF
+    assert_eq!(Error::Eof, walker.read_until('\n').unwrap_err());
+    walker.next();
+}
