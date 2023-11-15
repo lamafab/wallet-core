a5c8c5b656d73387cb5fc4b821a12397456b1200
lamafab <42901763+lamafab@users.noreply.github.com>
2023-11-15 14:43:57 +0000

    add ReceivePaymentRequest

---
+++ rust/tw_lightning/src/entry.rs
@@ -0,0 +1,14 @@
+use tw_proto::
+
+pub struct LightningEntry;
+
+impl LightningEntry {
+	#[inline]
+	pub fn process_node_state() {
+		todo!()
+	}
+
+	pub fn prepare_payment_request_invoice() {
+		todo!()
+	}
+}
